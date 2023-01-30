7f34e40c1d581f235c20c7a1fee3efc118baa75b
coreyloftus <coreyloftus@gmail.com>
2023-01-30 09:28:13 -0500

    Clip model created

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Clip(models.Model):
+    title = models.CharField(max_length=300)
+    body = models.CharField(max_length=10000)
+    difficulty = models.CharField(max_length=5, default=1)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['title']
