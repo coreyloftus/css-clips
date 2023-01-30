167d04fcd175234ff93308616d2f7d228239f42d
coreyloftus <coreyloftus@gmail.com>
2023-01-30 09:49:25 -0500

    Basic home route created, and GETtable

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import redirect
+from django.http import HttpResponse
+
+from .models import *
+from django.views import View
+from django.views.generic.base import TemplateView
 
 # Create your views here.
+
+
+class Home(TemplateView):
+    template_name = 'home.html'
