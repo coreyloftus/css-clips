9309a50492b12ac983d6b859b0ffbbc2554c32a4
coreyloftus <coreyloftus@gmail.com>
2023-01-30 11:07:42 -0500

    Added Clips Index to navbar, fixed urls route for home to blank instead of /

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('home/', views.Home.as_view(), name='home'),
+    path('', views.Home.as_view(), name='home'),
     path('about/', views.About.as_view(), name='about'),
     path('clips/', views.AllClips.as_view(), name="all_clips"),
 ]
