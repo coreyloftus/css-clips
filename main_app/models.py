3cce896265e11767856b5b39cbaf173e349f5d03
coreyloftus <coreyloftus@gmail.com>
2023-01-31 10:46:17 -0500

    add User model | add 'accounts/' path in urls.py | create login.html | update base.html for conditional login/logout buttons

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -10,6 +11,7 @@ class Clip(models.Model):
         max_length=10000, default='border:4mm ridge rgba(211, 220, 50, .6);')
     difficulty = models.CharField(max_length=5, default=1)
     created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
 
     def __str__(self):
         return self.title
